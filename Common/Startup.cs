86a198f797c6844906e3036b5df2265846b2c1dc
KonH <konh@yandex.ru>
2016-08-20 22:17:07 +0600

    Startup method to load template logics;

---
+++ Common/Startup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UDBase.Common {
+	public class Startup {
+		
+		// Using this method template logics can be loaded on first scene load
+		// And before any Awake/OnEnable/Start methods are invoked
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		static void OnApplicationStart() {
+			Debug.Log("On Application Start");
+		}
+	}
+}
