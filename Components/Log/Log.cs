eb95e3744997b02636aba8d4fd3ea03a898b716d
KonH <konh@yandex.ru>
2016-08-31 22:26:26 +0600

    Logger TODOs;

--- Components/Log/Log.cs
+++ Components/Log/Log.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 namespace UDBase.Components.Log {
 	public class Log : ComponentHelper<ILog> {
+		// TODO: Make full set of calls without memory allocation (code ganeration)
 
 		public static void Message(string msg) {
 			if(Instance != null) {
---
+++ Components/Log/Log_Visual.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UDBase.Components.Log {
+	public class Log_Visual : MonoBehaviour {
+		// TODO: Create Unity UI overlay and use it for logging
+
+		public void Init() {
+		}
+
+		public void Message(string msg)
+		{
+		}	
+	}
+}
