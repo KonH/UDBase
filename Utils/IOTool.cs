b71bfc0d85c22b801157ec15746028364fb12e11
KonH <konh@yandex.ru>
2016-09-11 20:24:10 +0600

    'Silent' arg for hide errors in IO methods added;

--- Utils/IOTool.cs
+++ Utils/IOTool.cs
@@ -5,7 +5,6 @@ using System.Collections;
 
 namespace UDBase.Utils {
 	public static class IOTool {
-		// TODO: Add silent
 
 		public static string GetPath(params string[] path) {
 			var result = path[0];
@@ -15,39 +14,47 @@ namespace UDBase.Utils {
 			return result;
 		}
 
-		public static bool CreateDirectory(string path) {
+		public static bool CreateDirectory(string path, bool silent = false) {
 			try {
 				Directory.CreateDirectory(path);
 				return true;
 			} catch (Exception e) {
-				Debug.LogErrorFormat("Exception while create directory at '{0}': {1}", path, e.ToString());
+				if( !silent ) {
+					Debug.LogErrorFormat("Exception while create directory at '{0}': {1}", path, e.ToString());
+				}
 				return false;
 			}
 		}
 
-		public static FileInfo[] GetDirFiles(string path, string searchPattern) {
+		public static FileInfo[] GetDirFiles(string path, string searchPattern, bool silent = false) {
 			try {
 				var dir = new DirectoryInfo(path);
 				return dir.GetFiles(searchPattern);
 			} catch (Exception e) {
-				Debug.LogErrorFormat("Exception while get files from directory at '{0}': {1}", path, e.ToString());
+				if( !silent ) {
+					Debug.LogErrorFormat("Exception while get files from directory at '{0}': {1}", path, e.ToString());
+				}
 				return new FileInfo[0];
 			}
 		}
 
-		public static void WriteAllText(string path, string contents) {
+		public static void WriteAllText(string path, string contents, bool silent = false) {
 			try {
 				File.WriteAllText(path, contents);
 			} catch (Exception e) {
-				Debug.LogErrorFormat("Exception while write text to '{0}': {1}", path, e.ToString());
+				if( !silent ) {
+					Debug.LogErrorFormat("Exception while write text to '{0}': {1}", path, e.ToString());
+				}
 			}
 		}
 
-		public static string ReadAllText(string path) {
+		public static string ReadAllText(string path, bool silent = false) {
 			try {
 				return File.ReadAllText(path);
 			} catch (Exception e) {
-				Debug.LogErrorFormat("Exception while read file from '{0}': {1}", path, e.ToString());
+				if( !silent ) {
+					Debug.LogErrorFormat("Exception while read file from '{0}': {1}", path, e.ToString());
+				}
 				return null;
 			}
 		}
@@ -63,33 +70,39 @@ namespace UDBase.Utils {
 			}
 		}
 
-		public static bool CopyFile(string originPath, string destinationPath) {
+		public static bool CopyFile(string originPath, string destinationPath, bool silent = false) {
 			try {
 				File.Copy(originPath, destinationPath);
 				return true;
 			} catch (Exception e) {
-				Debug.LogErrorFormat("Exception while copy file from '{0}' to {1}: {2}", 
-					originPath, destinationPath, e.ToString());
+				if( !silent ) {
+					Debug.LogErrorFormat("Exception while copy file from '{0}' to {1}: {2}", 
+						originPath, destinationPath, e.ToString());
+				}
 				return false;
 			}
 		}
 
-		public static bool CreateFile(string path) {
+		public static bool CreateFile(string path, bool silent = false) {
 			try {
 				File.Create(path);
 				return true;
 			} catch (Exception e) {
-				Debug.LogErrorFormat("Exception while create file '{0}': {1}", path, e.ToString());
+				if( !silent ) {
+					Debug.LogErrorFormat("Exception while create file '{0}': {1}", path, e.ToString());
+				}
 				return false;
 			}
 		}
 
-		public static bool DeleteFile(string path) {
+		public static bool DeleteFile(string path, bool silent = false) {
 			try {
 				File.Delete(path);
 				return true;
 			} catch (Exception e) {
-				Debug.LogErrorFormat("Exception while delete file from '{0}': {1}", path, e.ToString());
+				if( !silent ) {
+					Debug.LogErrorFormat("Exception while delete file from '{0}': {1}", path, e.ToString());
+				}
 				return false;
 			}
 		}
