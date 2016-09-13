c38b2d59ab8d4acf0c512891c6b6023d604e2be7
KonH <konh@yandex.ru>
2016-09-13 21:18:53 +0600

    Multi-components;

--- Components/Base/ComponentHelper.cs
+++ Components/Base/ComponentHelper.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace UDBase.Components {
-	
+	// TODO: Use one class for it?
 	/* Base class to make static helper for component:
 	 * Implement your component logics in 'ComponentX' class and make ComponentHelper<T> class 'Component'
 	 * After it you can call your component logics with 'Component.Instance'
@@ -11,7 +12,7 @@ namespace UDBase.Components {
 			if ( Instance != null ) {
 				Instance.DoSomething();
 			}
-		} 
+		}
 	*/
 	public abstract class ComponentHelperBase {
 		public abstract void Attach(IComponent handler);
@@ -22,6 +23,27 @@ namespace UDBase.Components {
 
 		public override void Attach(IComponent handler) {
 			Instance = (TComponent)handler;
+		}
+	}
+
+	/*
+	 * Another version of component helper provide you opportunity to use multiple instances
+	 * e.g. for call method on all instances or using custom switch:
+		public static void DoSomething() {
+			for(int i = 0; i < Instances.Count; i++) {
+				Instances[i].DoSomething();
+			}
+		}
+	 */
+	public class CompositeHelper<TComponent>: ComponentHelperBase where TComponent:IComponent {
+		public static List<TComponent> Instances { get; private set; }
+
+		public override void Attach(IComponent handler) {
+			var newHanlder = (TComponent)handler;
+			if( Instances == null ) {
+				Instances = new List<TComponent>();
+			}
+			Instances.Add(newHanlder);
 		}
 	}
 }
