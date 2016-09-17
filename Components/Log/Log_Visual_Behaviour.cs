9751010f08a222b06498b9a88a6cc4493d340a45
KonH <konh@yandex.ru>
2016-09-17 16:58:04 +0600

    Store messages in list to future filter; Clear method;

--- Components/Log/Log_Visual_Behaviour.cs
+++ Components/Log/Log_Visual_Behaviour.cs
@@ -1,16 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace UDBase.Components.Log {
 	public class Log_Visual_Behaviour : MonoBehaviour {
 		public Text Text;
 
+		List<string> _messages = new List<string>();
+
 		public void Init() {
+			Clear();
+		}
+
+		public void Clear() {
 			Text.text = "";
 		}
 
 		public void AddMessage(string msg) {
+			_messages.Add(msg);
+			ApplyMessage(msg);
+		}
+
+		void ApplyMessage(string msg) {
 			Text.text += msg + "\n";
 		}
 	}
