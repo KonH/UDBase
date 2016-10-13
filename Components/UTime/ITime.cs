82ded1e3cfecb2af418d3da66c4cc9a1718aad70
KonH <konh@yandex.ru>
2016-10-13 21:08:25 +0600

    Time Controller Proto Basics;

---
+++ Components/UTime/ITime.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace UDBase.Components.UTime {
+	public interface ITime : IComponent {
+		bool     IsAvailable { get; }
+		bool     IsFailed    { get; }
+		DateTime CurrentTime { get; }
+	}
+}
---
+++ Components/UTime/UTime.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace UDBase.Components.UTime {
+	public class UTime : ComponentHelper<ITime> {
+
+		public static bool IsStableTime() {
+			bool isStable = true;
+			for( int i = 0; i < Instances.Count; i++ ) {
+				var instance = Instances[i];
+				bool stable = instance.IsAvailable || instance.IsFailed;
+				isStable = isStable && stable;
+			}
+			return isStable;
+		}
+
+		public static DateTime CurrentTime() {
+			DateTime dt = default(DateTime);
+			for( int i = 0; i < Instances.Count; i++ ) {
+				var instance = Instances[i];
+				if( instance.IsAvailable ) {
+					dt = instance.CurrentTime;
+				}
+			}
+			return dt;
+		}
+	}
+}
