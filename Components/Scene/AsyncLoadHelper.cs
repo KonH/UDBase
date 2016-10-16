21414b5a81b8be1c8a6e6137f0ac38540e6369f7
KonH <konh@yandex.ru>
2016-10-16 14:52:31 +0700

    AsyncLoad prototype;

---
+++ Components/Scene/AsyncLoadHelper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace UDBase.Components.Scene {
+	public class AsyncLoadHelper : MonoBehaviour {
+
+		public void LoadScene(string name) {
+			StartCoroutine(LoadSceneCo(name));
+		}
+
+		IEnumerator LoadSceneCo(string name) {
+			yield return null;
+			var text = GameObject.FindObjectOfType<Text>();
+			var operation = SceneManager.LoadSceneAsync(name);
+			operation.allowSceneActivation = false;
+			while (!operation.isDone && operation.progress + Mathf.Epsilon < 0.9f ) {
+				if( text ) {
+					text.text = operation.progress.ToString();
+				}
+				yield return null;
+			}
+			operation.allowSceneActivation = true;
+		}
+	}
+}
---
+++ Components/Scene/AsyncSceneLoader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+// TODO: Use ResourcesLoader
+namespace UDBase.Components.Scene {
+	public class AsyncSceneLoader : IScene {
+		AsyncLoadHelper _helper       = null;
+		string          _loadingScene = "";
+
+		public AsyncSceneLoader(string loadingScene = "") {
+			_loadingScene = loadingScene;
+		}
+
+		public void Init() {
+			var go = new GameObject("temp");
+			GameObject.DontDestroyOnLoad(go);
+			_helper = go.AddComponent<AsyncLoadHelper>();
+		}
+
+		public void LoadScene(ISceneInfo sceneInfo) {
+			TryLoadLoadingScene(sceneInfo); 
+			_helper.LoadScene(sceneInfo.Name);
+		}
+
+		void TryLoadLoadingScene(ISceneInfo sceneInfo) {
+			var sceneName = GetLoadingSceneName(sceneInfo);
+			if( !string.IsNullOrEmpty(sceneName) ) {
+				SceneManager.LoadScene(sceneName);
+			}
+		}
+
+		protected virtual string GetLoadingSceneName(ISceneInfo sceneInfo) {
+			return _loadingScene;
+		}
+	}
+}
