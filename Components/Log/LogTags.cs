0aefe648c083c48c2c95c58fa015720d7df10771
KonH <konh@yandex.ru>
2016-10-26 18:48:33 +0700

    Add tag for Scene logs; Code alignment;

--- Components/Log/LogTags.cs
+++ Components/Log/LogTags.cs
@@ -5,18 +5,15 @@ namespace UDBase.Components.Log {
 	public class LogTags {
 		public const int Common = 1;
 		public const int UI     = 2;
+		public const int Scene  = 3;
 
-		string[] _names = new string[]{"Common", "UI"};
+		string[] _names = new string[]{"Common", "UI", "Scene"};
 
 		public virtual string GetName(int index) {
 			switch( index ) {
-				case Common: {
-						return "Common";
-				}
-
-				case UI: {
-						return "UI";
-				}
+				case Common : return "Common";
+				case UI     : return "UI";
+				case Scene  : return "Scene";
 			}
 			return "Unknown";
 		}
