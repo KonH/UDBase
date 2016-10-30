f048850bc6486ad04a8f4d136da88ba27e125b6f
KonH <konh@yandex.ru>
2016-10-30 11:32:20 +0700

    Simple SceneLoadButton;

---
+++ UI/Scene/SceneLoadButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UDBase.Controllers.Scene;
+
+namespace UDBase.Controllers.Scene.UI {
+	[RequireComponent(typeof(Button))]
+	public class SceneLoadButton : MonoBehaviour {
+		public string Name = "";
+
+		void Start () {
+			GetComponent<Button>().onClick.AddListener(OnClick);
+		}
+
+		void OnClick() {
+			Scene.LoadSceneInfo(new SceneName(Name));
+		}
+	}
+}
