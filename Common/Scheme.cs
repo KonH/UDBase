30341c9a6f1c7921655853947da8dda0585955dd
KonH <konh@yandex.ru>
2016-11-21 19:58:58 +0700

    Comments and error messages;

--- Common/Scheme.cs
+++ Common/Scheme.cs
@@ -4,22 +4,28 @@ using System.Collections.Generic;
 using UDBase.Controllers;
 
 namespace UDBase.Common {
-	/* If you want some combination of components you need to inherit from that class
-	 * add call AddComponent for required components
-	 */
+	/// <summary>
+	/// Scheme. If you want some combination of components you need to inherit from that class
+	/// add call AddComponent for required components
+	/// Note: This class used native Debug messages (Log controller is unreachable yet)
+	/// </summary>
 	public abstract class Scheme : IScheme {
 		Dictionary<IController, ControllerHelperBase> _controllers = new Dictionary<IController, ControllerHelperBase>();
 
 		public void AddController(ControllerHelperBase helper, params IController[] controllers) {
 			if( helper == null ) {
+				Debug.LogError("Helper can't be null!");
 				return;
 			}
 			if( (controllers == null) || (controllers.Length == 0) ) {
+				Debug.LogError("Controllers can't be null or empty!");
 				return;
 			}
 			for(int i = 0; i < controllers.Length; i++) {
 				if( controllers[i] != null ) {
 					_controllers.Add(controllers[i], helper);
+				} else {
+					Debug.LogWarning("Can't add null controler.");
 				}
 			}
 		}
