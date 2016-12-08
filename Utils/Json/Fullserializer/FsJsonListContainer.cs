aceda1ddbd1b893410c1a5a47e29be22f5329568
KonH <konh@yandex.ru>
2016-12-08 21:50:36 +0700

    Utility classes to work with Fullserializer;

---
+++ Utils/Json/Fullserializer/FsJsonListContainer.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FullSerializer;
+
+namespace UDBase.Utils.Json.Fullserializer {
+	public sealed class FsJsonListContainer {
+
+		FsJsonNodeContainer      _nodeContainer = null;
+		Dictionary<Type, string> _names         = new Dictionary<Type, string>();
+
+		fsSerializer _serializer = new fsSerializer();
+
+		Dictionary<string, Dictionary<string, object>> _nodeCache 
+			= new Dictionary<string, Dictionary<string, object>>();
+
+		public FsJsonListContainer(FsJsonNodeContainer nodeContainer) {
+			_nodeContainer = nodeContainer;
+		}
+
+		public FsJsonListContainer(FsJsonNodeContainer nodeContainer, Dictionary<Type, string> names):
+			this(nodeContainer) {
+			_names = names;
+		}
+
+		public void Add<T>(string name) {
+			_names.Add(typeof(T), name);
+		}
+
+		public T LoadItem<T>(string nodeName, string itemName) {
+			Dictionary<string, object> cachedNode;
+			if( _nodeCache.TryGetValue(nodeName, out cachedNode) ) {
+				object value;
+				cachedNode.TryGetValue(itemName, out value);
+				return (T)value;
+			} else {
+				var node = _nodeContainer.LoadNode(nodeName);
+				if( node != null ) {
+					var nodeContent = node.AsDictionary;
+					CacheContent<T>(nodeName, nodeContent);
+					if( nodeContent != null ) {
+						fsData fsValue;
+						nodeContent.TryGetValue(itemName, out fsValue);
+						T value = default(T);
+						_serializer.TryDeserialize(fsValue, ref value);
+						return value;
+					}
+				}
+			}
+			return default(T);
+		}
+
+		void CacheContent<T>(string nodeName, Dictionary<string, fsData> nodeContent) {
+			var nodeContentCache = new Dictionary<string, object>();
+			var nodeContentIter = nodeContent.GetEnumerator();
+			while( nodeContentIter.MoveNext() ) {
+				var current = nodeContentIter.Current;
+				T value = default(T);	
+				_serializer.TryDeserialize(current.Value, ref value);
+				nodeContentCache.Add(current.Key, value);
+			}
+			_nodeCache.Add(nodeName, nodeContentCache);
+		}
+
+		public T LoadItem<T>(string itemName) {
+			string nodeName;
+			if( _names.TryGetValue(typeof(T), out nodeName) ) {
+				return LoadItem<T>(nodeName, itemName);
+			}
+			return default(T);
+		}
+	}
+}
---
+++ Utils/Json/Fullserializer/FsJsonNodeContainer.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FullSerializer;
+
+namespace UDBase.Utils.Json.Fullserializer {
+	public sealed class FsJsonNodeContainer {
+		Dictionary<string, fsData>  _nodes = null;
+		Dictionary<Type, string>    _names = new Dictionary<Type, string>();
+		Dictionary<Type, object>    _cache = new Dictionary<Type, object>();
+
+		fsSerializer _serializer = new fsSerializer();
+
+		public FsJsonNodeContainer(string content) {
+			if( !string.IsNullOrEmpty(content) ) {
+				var data = fsJsonParser.Parse(content);
+				_nodes = data.AsDictionary;
+			}
+			if( _nodes == null ) {
+				_nodes = new Dictionary<string, fsData>();
+			}
+		}
+
+		public FsJsonNodeContainer(string content, Dictionary<Type, string> names):this(content) {
+			_names = names;
+		}
+
+		public void Add<T>(string name) {
+			_names.Add(typeof(T), name);
+		}
+
+		public fsData LoadNode(string name) {
+			fsData value;
+			_nodes.TryGetValue(name, out value);
+			if( value != null ) {
+				return value;
+			}
+			return null;
+		}
+
+		public T LoadNode<T>() {
+			var type = typeof(T);
+			object value;
+			if( !_cache.TryGetValue(type, out value) ) {
+				string key;
+				if( _names.TryGetValue(type, out key) ) {
+					value = LoadNode<T>(key);
+					_cache.Add(type, value);
+				}
+			}
+			return (T)value;
+		}
+
+		public T LoadNode<T>(string name) {
+			var node = LoadNode(name);
+			if( node != null ) {
+				T instance = default(T); 
+				_serializer.TryDeserialize(node, ref instance);
+				return instance;
+			}
+			return default(T);
+		}
+
+		public void SaveNode<T>(T node) {
+			var type = typeof(T);
+			SaveNode(node, _names[type]);
+		}
+
+		public void SaveNode<T>(T node, string name) {
+			fsData data;
+			_serializer.TrySerialize(node, out data);
+			_nodes.Remove(name);
+			_nodes.Add(name, data);
+			var type = typeof(T);
+			_cache.Remove(type);
+			_cache.Add(type, node);
+		}
+
+		public string GetNodesContent(bool prettyJson) {
+			var data = new fsData(_nodes);
+			return prettyJson ? fsJsonPrinter.PrettyJson(data) : fsJsonPrinter.CompressedJson(data);
+		}
+	}
+}
