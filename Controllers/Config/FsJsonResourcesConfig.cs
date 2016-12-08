37ade228a6bc0e6f43e2848c15f5b18c3d5847e9
KonH <konh@yandex.ru>
2016-12-08 21:52:39 +0700

    Config and save implementations uses Fullserializer;

---
+++ Controllers/Config/FsJsonResourcesConfig.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UDBase.Common;
+using UDBase.Controllers;
+using UDBase.Utils.Json.Fullserializer;
+
+namespace UDBase.Controllers.ConfigSystem {
+	public sealed class FsJsonResourcesConfig : IConfig {
+		string                   _fileName      = "";
+		FsJsonNodeContainer      _nodeContainer = null;
+		FsJsonListContainer      _listContainer = null;
+		Dictionary<Type, string> _nodeNames     = new Dictionary<Type, string>();
+		Dictionary<Type, string> _listNames     = new Dictionary<Type, string>();
+
+		public FsJsonResourcesConfig() {
+			_fileName = UDBaseConfig.JsonConfigName;
+		}
+
+		public FsJsonResourcesConfig(string fileName) {
+			_fileName = fileName;
+		}
+
+		public void Init() {
+			var config = Resources.Load(_fileName) as TextAsset;
+			if( config ) {
+				var configContent = config.text;
+				_nodeContainer = new FsJsonNodeContainer(configContent, _nodeNames);
+				_listContainer = new FsJsonListContainer(_nodeContainer, _listNames);
+			} else {
+				Debug.LogErrorFormat(
+					"JsonResourcesConfig: Can't read config file from Resources/{0}", 
+					_fileName);
+			}
+		}
+
+		public void PostInit() {}
+
+		public FsJsonResourcesConfig AddNode<T>(string name) {
+			if( _nodeContainer == null ) {
+				_nodeNames.Add(typeof(T), name);
+			} else {
+				_nodeContainer.Add<T>(name);
+			}
+			return this;
+		}
+
+		public FsJsonResourcesConfig AddList<T>(string name) {
+			if( _listContainer == null ) {
+				_listNames.Add(typeof(T), name);
+			} else {
+				_listContainer.Add<T>(name);
+			}
+			return this;
+		}
+
+		public T GetNode<T>() {
+			if( _nodeContainer != null ) {
+				return _nodeContainer.LoadNode<T>();
+			}
+			return default(T);
+		}
+
+		public T GetItem<T>(string name) {
+			if( _listContainer != null ) {
+				return _listContainer.LoadItem<T>(name);
+			}
+			return default(T);
+		}
+	}
+}
---
+++ Controllers/Save/FsJsonDataSave.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UDBase.Common;
+using UDBase.Controllers;
+using UDBase.Utils;
+using UDBase.Utils.Json.Fullserializer;
+
+namespace UDBase.Controllers.SaveSystem {
+	public sealed class FsJsonDataSave:ISave {
+
+		bool                     _prettyJson = false;
+		string                   _fileName   = "";
+		string                   _filePath   = "";
+		FsJsonNodeContainer      _container  = null;
+		Dictionary<Type, string> _names      = new Dictionary<Type, string>();
+
+		public FsJsonDataSave():this(false, UDBaseConfig.JsonSaveName) {}
+
+		public FsJsonDataSave(bool prettyJson):this(prettyJson, UDBaseConfig.JsonSaveName) {}
+
+		public FsJsonDataSave(string fileName):this(false, UDBaseConfig.JsonSaveName) {}
+
+		public FsJsonDataSave(bool prettyJson, string fileName) {
+			_prettyJson = prettyJson;
+			_fileName   = fileName;
+		}
+
+		public void Init() {
+			_filePath = IOTool.GetPath(Application.persistentDataPath, _fileName);
+			if( !TryLoadContainer() ) {
+				Debug.LogWarningFormat(
+					"JsonDataSave: Can't read save file from {0}, re-create it.", 
+					_fileName);
+				IOTool.CreateFile(_filePath);
+				TryLoadContainer();
+			}
+		}
+
+		public void PostInit() {}
+
+		bool TryLoadContainer() {
+			if( _container == null ) {
+				var saveContent = IOTool.ReadAllText(_filePath, true);
+				if( saveContent != null ) {
+					_container = new FsJsonNodeContainer(saveContent, _names);
+					return true;
+				}
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		public FsJsonDataSave AddNode<T>(string name) {
+			if( _container == null ) {
+				_names.Add(typeof(T), name);
+			} else {
+				_container.Add<T>(name);
+			}
+			return this;
+		}
+
+		public T GetNode<T>() {
+			TryLoadContainer();
+			if( _container != null ) {
+				return _container.LoadNode<T>();
+			}
+			return default(T);
+		}
+
+		public void SaveNode<T>(T node) {
+			TryLoadContainer();
+			if( _container != null ) {
+				_container.SaveNode(node);
+				var content = _container.GetNodesContent(_prettyJson);
+				IOTool.WriteAllText(_filePath, content);
+			}
+		}
+			
+		public void Clear() {
+			IOTool.DeleteFile(_filePath);
+		}
+	}
+}
