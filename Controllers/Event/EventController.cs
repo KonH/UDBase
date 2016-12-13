5b478d3b4283aea179884873c78dad0207bce57c
KonH <konh@yandex.ru>
2016-12-13 20:50:32 +0700

    EventSystem: API;

---
+++ Controllers/Event/EventController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UDBase.Controllers.EventSystem {
+	public class EventController : IEvent {
+
+		public void Init() {}
+
+		public void PostInit() {}
+
+		public void Fire<T>(T arg) {
+			Debug.LogFormat("Fire: {0}", arg);
+		}
+
+		public void Subscribe<T>(T eventType, object handler, System.Action<T> callback) {
+			Debug.LogFormat("Subscribe: {0} for {1}", eventType, handler);
+		}
+
+		public void Unsubscribe<T>(T eventType, object handler) {
+			Debug.LogFormat("Unsubscribe: {0} from {1}", eventType, handler);
+		}
+	}
+}
---
+++ Controllers/Event/IEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UDBase.Controllers.EventSystem {
+	public interface IEvent:IController {
+
+		void Fire<T>(T arg);
+		void Subscribe<T>(T eventType, object handler, Action<T> callback);
+		void Unsubscribe<T>(T eventType, object handler);
+	}
+}
