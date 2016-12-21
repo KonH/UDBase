50068b5468d81fc9d5986fed90ba942c41f0af5d
KonH <konh@yandex.ru>
2016-12-21 19:47:21 +0700

    Editor AssetUtility helper;

---
+++ Editor/Tools/AssetUtility.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace UDBase.Editor {
+	public static class AssetUtility {
+
+		public static T CreateAsset<T>() where T : ScriptableObject {
+			T asset = ScriptableObject.CreateInstance<T>();
+			string path = AssetDatabase.GetAssetPath (Selection.activeObject);
+			if ( path == "" ) {
+				path = "Assets";
+			} else if ( Path.GetExtension (path) != "" ) {
+				path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+			}
+			string assetPathAndName = path + "/" + typeof(T).ToString() + ".asset";
+			assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(assetPathAndName);
+			AssetDatabase.CreateAsset(asset, assetPathAndName);
+			SaveAndFocusAsset(asset);
+			return asset;
+		}
+
+		public static T AddSubAsset<T>(ScriptableObject parent) where T:ScriptableObject {
+			T asset = ScriptableObject.CreateInstance<T>();
+			AssetDatabase.AddObjectToAsset(asset, parent);
+			SaveAndFocusAsset(asset);
+			return asset;
+		}
+
+		public static void SaveAndFocusAsset<T>(T asset) where T:ScriptableObject {
+			SaveAssets();
+			FocusAsset(asset);
+		}
+
+		public static void SaveAssets() {
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+		}
+
+		public static void FocusAsset<T>(T asset) where T:ScriptableObject {
+			EditorUtility.FocusProjectWindow();
+			Selection.activeObject = asset;
+		}
+	}
+}
