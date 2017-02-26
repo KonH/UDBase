cc182293139e43e29bb880b491438d7f1dfa19a6
KonH <konh@yandex.ru>
2017-02-26 16:35:03 +0700

    PlayerPrefsUtils: class with GetBool method;

---
+++ Utils/PlayerPrefsUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UDBase.Utils {
+	public static class PlayerPrefsUtils {
+		public static void SetBool(string key, bool value, bool save = true) {
+			PlayerPrefs.SetInt(key, value ? 1 : 0);
+			if( save ) {
+				PlayerPrefs.Save();
+			}
+		}
+
+		public static bool GetBool(string key, bool defaultValue) {
+			var value = PlayerPrefs.GetInt(key, defaultValue ? 1 : 0);
+			return (value == 1);
+		}
+	}
+}
