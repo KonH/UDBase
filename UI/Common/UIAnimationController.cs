9d1d721cc1380a2a2091334cbf1d2829ff5a1203
KonH <konh@yandex.ru>
2017-02-26 13:38:27 +0700

    UI: Now you can use multiple UIAnimations on one UIElement using UIAnimationController (step-by-step or parallel);

--- UI/Common/UIAnimationController.cs
+++ UI/Common/UIAnimationController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 namespace UDBase.UI.Common {
     public class UIAnimationController : MonoBehaviour, IShowAnimation, IHideAnimation, IClearAnimation {
-
+		public bool StepByStep = true;
 		public List<UIShowHideAnimation> ShowSteps = new List<UIShowHideAnimation>();
 		public List<UIShowHideAnimation> HideSteps = new List<UIShowHideAnimation>();
+
+		List<UIShowHideAnimation> _showTemp = new List<UIShowHideAnimation>();
+		List<UIShowHideAnimation> _hideTemp = new List<UIShowHideAnimation>();
 
 		public void Clear() {
 			for( int i = 0; i < ShowSteps.Count; i++ ) {
@@ -18,8 +21,15 @@ namespace UDBase.UI.Common {
         }
 
 		public void Show(UIElement element, Action action) {
-			SetShown();
-			ShowStep(0, element, action);
+			if( StepByStep ) {
+				ShowStep(0, element, action);
+			} else {
+				_showTemp.AddRange(ShowSteps);
+				for( int i = 0; i < ShowSteps.Count; i++ ) {
+					var anim = ShowSteps[i];
+					anim.Show(element, () => ShowParallelCallback(anim, action));
+				}
+			}
         }
 
 		void ShowStep(int index, UIElement element, Action callback) {
@@ -30,21 +40,42 @@ namespace UDBase.UI.Common {
 			}
 		}
 
-        public void SetShown() {
-			for( int i = 0; i < HideSteps.Count; i++ ) {
-				HideSteps[i].SetShown();
+		void ShowParallelCallback(UIShowHideAnimation anim, Action callback) {
+			_showTemp.Remove(anim);
+			if( _showTemp.Count == 0 ) {
+				callback();
 			}
-        }
+		}
+
+        public void SetShown() {
+			for( int i = 0; i < ShowSteps.Count; i++ ) {
+				ShowSteps[i].SetShown();
+			}
+		}
 
         public void Hide(UIElement element, Action action) {
-			SetHidden();
-			HideStep(0, element, action);
+			if( StepByStep ) {
+				HideStep(0, element, action);
+			} else {
+				_hideTemp.AddRange(HideSteps);
+				for( int i = 0; i < HideSteps.Count; i++ ) {
+					var anim = HideSteps[i];
+					anim.Hide(element, () => HideParallelCallback(anim, action));
+				}
+			}
         }
 
 		void HideStep(int index, UIElement element, Action callback) {
 			if( index < HideSteps.Count ) {
-				HideSteps[index].Show(element, () => HideStep(index + 1, element, callback));
+				HideSteps[index].Hide(element, () => HideStep(index + 1, element, callback));
 			} else {
+				callback();
+			}
+		}
+
+		void HideParallelCallback(UIShowHideAnimation anim, Action callback) {
+			_hideTemp.Remove(anim);
+			if( _hideTemp.Count == 0 ) {
 				callback();
 			}
 		}
@@ -53,8 +84,6 @@ namespace UDBase.UI.Common {
 			for( int i = 0; i < HideSteps.Count; i++ ) {
 				HideSteps[i].SetHidden();
 			}
-        }
-
-        
+		}
     }
 }
