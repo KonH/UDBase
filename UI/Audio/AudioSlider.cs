ee2b1ea0d90e9beb2fb59bd387b8eeb93051b678
KonH <konh@yandex.ru>
2017-05-29 23:25:21 +0700

    Audio UI elements: volume slider, events usage;

---
+++ UI/Audio/AudioSlider.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UDBase.Controllers.EventSystem;
+
+namespace UDBase.Controllers.AudioSystem.UI {
+	[RequireComponent(typeof(Slider))]
+	public class AudioSlider : MonoBehaviour {
+
+		public string ChannelName  = null;
+		public bool   DefaultSound = false;
+		public bool   DefaultMusic = false;
+
+		Slider _slider = null;
+
+		void OnEnable() {
+			Events.Subscribe<VolumeChangeEvent>(this, OnVolumeChanged);
+		}
+
+		void OnDisable() {
+			Events.Unsubscribe<VolumeChangeEvent>(OnVolumeChanged);
+		}
+
+		void Start() {
+			_slider = GetComponent<Slider>();
+			SetupChannelName();
+			_slider.value = Audio.GetChannelVolume(ChannelName);
+			_slider.onValueChanged.AddListener(OnValueChanged);
+		}
+
+		void SetupChannelName() {
+			if ( DefaultSound ) {
+				ChannelName = Audio.Default_Sound_Channel_Volume;
+			}
+			if ( DefaultMusic ) {
+				ChannelName = Audio.Default_Music_Channel_Volume;
+			}
+		}
+
+		void OnVolumeChanged(VolumeChangeEvent e) {
+			if ( e.Channel == ChannelName ) {
+				_slider.value = e.Volume;
+			}
+		}
+
+		void OnValueChanged(float value) {
+			var curValue = Audio.GetChannelVolume(ChannelName);
+			if ( !Mathf.Approximately(value, curValue) ) {
+				Audio.SetChannelVolume(ChannelName, value);
+			}
+		}
+	}
+}
--- UI/Audio/AudioToggleButton.cs
+++ UI/Audio/AudioToggleButton.cs
@@ -1,33 +1,61 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UDBase.Controllers.EventSystem;
 
 namespace UDBase.Controllers.AudioSystem.UI {
 	[RequireComponent(typeof(Button))]
 	public class AudioToggleButton : MonoBehaviour {
 
-		public string VolumeParameter  = null;
-		public bool   DefaultSound     = false;
-		public bool   DefaultMusic     = false;
+		public GameObject ActiveItem  = null;
+		public GameObject MutedItem   = null;
+		public string    ChannelName  = null;
+		public bool      DefaultSound = false;
+		public bool      DefaultMusic = false;
 
 		Button _button = null;
 
-		void Awake() {
+		void OnEnable() {
+			Events.Subscribe<VolumeChangeEvent>(this, OnVolumeChanged);
+		}
+
+		void OnDisable() {
+			Events.Unsubscribe<VolumeChangeEvent>(OnVolumeChanged);
+		}
+
+		void Start() {
 			_button = GetComponent<Button>();
 			_button.onClick.AddListener(OnClick);
 			SetupChannelName();
+			UpdateState();
 		}
 
 		void SetupChannelName() {
 			if ( DefaultSound ) {
-				VolumeParameter = Audio.Default_Sound_Channel_Volume;
+				ChannelName = Audio.Default_Sound_Channel_Volume;
 			}
 			if ( DefaultMusic ) {
-				VolumeParameter = Audio.Default_Music_Channel_Volume;
+				ChannelName = Audio.Default_Music_Channel_Volume;
+			}
+		}
+
+		void UpdateState() {
+			var muted = Audio.IsChannelMuted(ChannelName);
+			if ( ActiveItem ) {
+				ActiveItem.SetActive(!muted);
+			}
+			if ( MutedItem ) {
+				MutedItem.SetActive(muted);
+			}
+		}
+
+		void OnVolumeChanged(VolumeChangeEvent e) {
+			if ( e.Channel == ChannelName ) {
+				UpdateState();
 			}
 		}
 
 		void OnClick() {
-			Audio.ToggleChannel(VolumeParameter);
+			Audio.ToggleChannel(ChannelName);
 		}
 	}
 }
