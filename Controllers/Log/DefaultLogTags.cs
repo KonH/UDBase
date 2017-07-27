8dfe7f132ea1628b980fd8cba56d2fff7b4bf4e4
KonH <konh@yandex.ru>
2017-07-27 21:51:35 +0700

    Log tags initial changes;

---
+++ Controllers/Log/DefaultLogTags.cs
@@ -0,0 +1,20 @@
+﻿using UDBase.Utils;
+
+namespace UDBase.Controllers.LogSystem {
+	[CompositeEnum(typeof(NewLogTags))]
+	public enum DefaultLogTags {
+		Common      = 1,
+		UI          = 2,
+		Scene       = 3,
+		Inventory   = 4,
+		Config      = 5,
+		Save        = 6,
+		Json        = 7,
+		Event       = 8,
+		Content     = 9,
+		Time        = 10,
+		Network     = 11,
+		Leaderboard = 12,
+		Audio       = 13,
+	}
+}
