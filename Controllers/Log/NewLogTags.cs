8fd7e18a355c45beb5efb1a700f79c6d49ad5805
KonH <konh@yandex.ru>
2017-07-27 21:56:12 +0700

    New base log tags added;

---
+++ Controllers/Log/NewLogTags.cs
@@ -0,0 +1,16 @@
+public enum NewLogTags {
+	Common = 1,
+	UI = 2,
+	Scene = 3,
+	Inventory = 4,
+	Config = 5,
+	Save = 6,
+	Json = 7,
+	Event = 8,
+	Content = 9,
+	Time = 10,
+	Network = 11,
+	Leaderboard = 12,
+	Audio = 13,
+	CustomTag = 101
+}
