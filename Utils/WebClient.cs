0fbdb1b5982fe58b12002dfd1864dc3642b4c779
KonH <konh@yandex.ru>
2017-10-11 22:19:22 +0700

    WebClient improvements: ApplyAuthHeader, InProgress/HasAuthorization flags;

--- Utils/WebClient.cs
+++ Utils/WebClient.cs
@@ -6,8 +6,26 @@ namespace UDBase.Utils {
 
 		const string AuthHeaderName = "Authorization";
 
+		public bool InProgress {
+			get {
+				return _requestCount > 0;
+			}
+		}
+
+		public bool HasAuthorization {
+			get {
+				return !string.IsNullOrEmpty(_authHeaderValue);
+			}
+		}
+
 		string                     _authHeaderValue = null;
 		Dictionary<string, string> _authHeaderOnly  = null;
+		int                        _requestCount    = 0;
+
+		public WebClient() {
+			_authHeaderValue = "";
+			_authHeaderOnly = new Dictionary<string, string>();
+		}
 
 		public WebClient(string userName, string userPassword) {
 			_authHeaderValue = NetUtils.CreateBasicAuthorization(userName, userPassword);
@@ -24,14 +42,25 @@ namespace UDBase.Utils {
 			}
 		}
 
+		public void ApplyAuthHeader(string value) {
+			_authHeaderValue = value;
+			_authHeaderOnly = UpdateHeaders(new Dictionary<string, string>());
+		}
+
 		public void SendGetRequest(
 			string url,
 			float timeout = NetUtils.DefaultTimeout,
 			Dictionary<string, string> headers = null,
 			Action<NetUtils.Response> onComplete = null)
 		{
+			_requestCount++;
 			headers = UpdateHeaders(headers);
-			NetUtils.SendGetRequest(url, timeout, headers, onComplete);
+			NetUtils.SendGetRequest(url, timeout, headers, resp => {
+				_requestCount--;
+				if ( onComplete != null ) {
+					onComplete(resp);
+				}
+			});
 		}
 
 		public void SendPostRequest(
@@ -41,8 +70,14 @@ namespace UDBase.Utils {
 			Dictionary<string, string> headers = null,
 			Action<NetUtils.Response> onComplete = null)
 		{
+			_requestCount++;
 			headers = UpdateHeaders(headers);
-			NetUtils.SendPostRequest(url, data, timeout, headers, onComplete);
+			NetUtils.SendPostRequest(url, data, timeout, headers, resp => {
+				_requestCount--;
+				if ( onComplete != null ) {
+					onComplete(resp);
+				}
+			});
 		}
 
 		public void SendJsonPostRequest(
@@ -50,9 +85,16 @@ namespace UDBase.Utils {
 			string data,
 			float timeout = NetUtils.DefaultTimeout,
 			Dictionary<string, string> headers = null,
-			Action<NetUtils.Response> onComplete = null) {
+			Action<NetUtils.Response> onComplete = null)
+		{
+			_requestCount++;
 			headers = UpdateHeaders(headers);
-			NetUtils.SendJsonPostRequest(url, data, timeout, headers, onComplete);
+			NetUtils.SendJsonPostRequest(url, data, timeout, headers, resp => {
+				_requestCount--;
+				if ( onComplete != null ) {
+					onComplete(resp);
+				}
+			});
 		}
 	}
 }
