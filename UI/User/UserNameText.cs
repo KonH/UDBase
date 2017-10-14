d6ea817d7c9059fc945c6c72c2e759c7b879fdea
KonH <konh@yandex.ru>
2017-10-14 18:59:50 +0700

    UI: User name text;

---
+++ UI/User/UserNameText.cs
@@ -0,0 +1,29 @@
+﻿using UDBase.Controllers.EventSystem;
+using UDBase.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UDBase.Controllers.UserSystem.UI {
+	[RequireComponent(typeof(Text))]
+	public class UserNameText : MonoBehaviour {
+		Text _text;
+
+		void Start() {
+			_text = GetComponent<Text>();
+			UpdateValue(User.Name);
+			Events.Subscribe<User_NameChange>(this, OnUserNameChange);
+		}
+
+		void OnDestroy() {
+			Events.Unsubscribe<User_NameChange>(OnUserNameChange);
+		}
+
+		void OnUserNameChange(User_NameChange e) {
+			UpdateValue(e.Name);
+		}
+
+		void UpdateValue(string value) {
+			_text.text = TextUtils.EnsureString(value);
+		}
+	}
+}
