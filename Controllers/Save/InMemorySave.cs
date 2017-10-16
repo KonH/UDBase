2857b87b4d990afb84bb33fc3a2e73893174aa72
KonH <konh@yandex.ru>
2017-10-17 00:32:48 +0700

    Feature: InMemorySave;

---
+++ Controllers/Save/InMemorySave.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UDBase.Controllers.SaveSystem {
+	public sealed class InMemorySave:ISave {
+		public Dictionary<Type, object> _state = new Dictionary<Type, object>();
+
+		public void Init() {}
+		public void PostInit() {}
+		public void Reset() {}
+
+		public InMemorySave AddNode<T>(string name) {
+			var instance = Activator.CreateInstance<T>();
+			_state.Add(typeof(T), instance);
+			return this;
+		}
+
+		public T GetNode<T>(bool autoFill) {
+			return (T)_state[typeof(T)];
+		}
+
+		public void SaveNode<T>(T node) {}
+			
+		public void Clear() {}
+	}
+}
