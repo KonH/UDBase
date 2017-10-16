c04d53e188d342095c71fe5d55d5ddf078c2ca6a
KonH <konh@yandex.ru>
2017-10-16 23:38:57 +0700

    Feature: JsonUtils;

---
+++ Utils/Json/JsonUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UDBase.Controllers.LogSystem;
+using FullSerializer;
+
+namespace UDBase.Utils.Json {
+	public static class JsonUtils {
+		public static T Serialize<T>(string json) where T:new() {
+			var serializer = new fsSerializer();
+			try {
+				var data = fsJsonParser.Parse(json);
+				var result = new T();
+				serializer.TryDeserialize(data, ref result);
+				return result;
+			} catch ( Exception e ) {
+				Log.ErrorFormat("Serialize: exception: {0}", LogTags.Common, e);
+			}
+			return default(T);
+		}
+	}
+}
