c9163edc9d012a8b4b3b873089bad061fe0833a8
KonH <konh@yandex.ru>
2018-02-12 20:54:01 +0700

    Add documentation generation to .csproj via editor menu;

---
+++ Scripts/Editor/Tools/CSprojUpdater.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml;
+
+namespace UDBase.EditorTools {
+	public static class CSProjUpdater {
+		
+		public static void UpdateCsprojFile(string fileName) {
+			var doc = new XmlDocument();
+			doc.Load(fileName);
+
+			// <PropertyGroup>
+			//    <DocumentationFile>$(OutputPath)Assembly-CSharp.xml</DocumentationFile>
+			// </PropertyGroup>
+			var namespaceUri = doc.LastChild.Attributes["xmlns"].Value;
+			var group = doc.CreateElement("PropertyGroup", namespaceUri);
+			var docFile = doc.CreateElement("DocumentationFile", namespaceUri);
+			docFile.InnerText = "$(OutputPath)Assembly-CSharp.xml";
+			group.AppendChild(docFile);
+
+			doc.LastChild.AppendChild(group);
+
+			doc.Save(fileName);
+
+			UnityEngine.Debug.Log($"Xml documentation generation added to project '{fileName}'");
+		}
+	}
+}
