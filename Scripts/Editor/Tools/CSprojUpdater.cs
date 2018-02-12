4217cd5a2810df2d9d88f02fbbd240220887e546
KonH <konh@yandex.ru>
2018-02-12 22:30:01 +0700

    Add post-build event to update docs;

--- Scripts/Editor/Tools/CSprojUpdater.cs
+++ Scripts/Editor/Tools/CSprojUpdater.cs
@@ -18,9 +18,36 @@ namespace UDBase.EditorTools {
 
 			doc.LastChild.AppendChild(group);
 
+			UnityEngine.Debug.Log($"Xml documentation generation added to project '{fileName}'");
+
+			// <Target Name="PostBuild" AfterTargets="PostBuildEvent">
+			//    <Exec Command="call cd $(SolutionDir)Tools\&#xD;&#xA;call &quot;$(SolutionDir)Tools\GenerateDocs.bat&quot;" />
+			// </Target>
+			var target = doc.CreateElement("Target", namespaceUri);
+			{
+				var nameAttr = doc.CreateAttribute("Name");
+				nameAttr.Value = "PostBuild";
+				target.Attributes.Append(nameAttr);
+
+				var targetAttr = doc.CreateAttribute("AfterTargets");
+				targetAttr.Value = "PostBuildEvent";
+				target.Attributes.Append(targetAttr);
+
+				var exec = doc.CreateElement("Exec", namespaceUri);
+				{
+
+					var commandAttr = doc.CreateAttribute("Command");
+					commandAttr.Value = "call cd $(SolutionDir)Tools\ncall \"$(SolutionDir)Tools\\GenerateDocs.bat\"";
+					exec.Attributes.Append(commandAttr);
+				}
+				target.AppendChild(exec);
+			}
+			doc.LastChild.AppendChild(target);
+
+			UnityEngine.Debug.Log($"Markdown post-process added to project '{fileName}'");
+
 			doc.Save(fileName);
 
-			UnityEngine.Debug.Log($"Xml documentation generation added to project '{fileName}'");
 		}
 	}
 }
