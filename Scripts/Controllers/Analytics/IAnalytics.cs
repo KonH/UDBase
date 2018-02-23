c328e070cff7207a8a38bc514feeb4f748212a55
KonH <konh@yandex.ru>
2018-02-23 23:26:11 +0700

    Analytics stubs;

---
+++ Scripts/Controllers/Analytics/IAnalytics.cs
@@ -0,0 +1,4 @@
+﻿namespace UDBase.Controllers.AnalyticsSystem {
+	public interface IAnalytics {
+	}
+}
---
+++ Scripts/Controllers/Analytics/UnityAnalyticsController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.Analytics;
+using UDBase.Controllers.LogSystem;
+
+namespace UDBase.Controllers.AnalyticsSystem {
+	public class UnityAnalyticsController : IAnalytics, ILogContext {
+
+		ILog _log;
+
+		public UnityAnalyticsController(ILog log) {
+			_log = log;
+		}
+	}
+}
