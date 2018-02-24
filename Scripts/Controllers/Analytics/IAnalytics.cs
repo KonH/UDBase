81c56d097c31887b8a584da83597112426fb29dd
KonH <konh@yandex.ru>
2018-02-24 11:40:55 +0700

    Analytics basics;

--- Scripts/Controllers/Analytics/IAnalytics.cs
+++ Scripts/Controllers/Analytics/IAnalytics.cs
@@ -1,4 +1,20 @@
-﻿namespace UDBase.Controllers.AnalyticsSystem {
+﻿using System.Collections.Generic;
+
+namespace UDBase.Controllers.AnalyticsSystem {
+
+	/// <summary>
+	/// Analytics system allows you to track analytics events without using concrete analytics API
+	/// </summary>
 	public interface IAnalytics {
+
+		/// <summary>
+		/// Raise event without additional data
+		/// </summary>
+		void Event(string eventName);
+
+		/// <summary>
+		/// Raise event with additional data dictionary
+		/// </summary>
+		void Event(string eventName, Dictionary<string, object> data);
 	}
 }
--- Scripts/Controllers/Analytics/UnityAnalyticsController.cs
+++ Scripts/Controllers/Analytics/UnityAnalyticsController.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine.Analytics;
+﻿using System.Text;
+using System.Collections.Generic;
+using UnityEngine.Analytics;
+using UDBase.Utils;
 using UDBase.Controllers.LogSystem;
 
 namespace UDBase.Controllers.AnalyticsSystem {
@@ -8,6 +11,23 @@ namespace UDBase.Controllers.AnalyticsSystem {
 
 		public UnityAnalyticsController(ILog log) {
 			_log = log;
+		}
+
+		public void Event(string eventName) {
+			var result = Analytics.CustomEvent(eventName);
+			_log.MessageFormat(this, "Fired event: '{0}', result: {1}", eventName, result);
+		}
+
+		public void Event(string eventName, Dictionary<string, object> data) {
+			var result = Analytics.CustomEvent(eventName, data);
+			var getDataInfo = new StringFunctor(() => {
+				var dataSb = new StringBuilder();
+				foreach (var item in data) {
+					dataSb.AppendFormat("'{0}' => '{1}';", item.Key, item.Value);
+				}
+				return dataSb.ToString();
+			});
+			_log.MessageFormat(this, "Fired event: '{0}' with data: {1}, result: {2}", eventName, getDataInfo, result);
 		}
 	}
 }
