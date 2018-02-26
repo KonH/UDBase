5698b3aeb24b048591e3c0e80aa43538f7d4b9df
KonH <konh@yandex.ru>
2018-02-26 18:53:53 +0700

    Extensions: OneLine updated;
    https://github.com/slavniyteo/one-line
    de95bdec4f0dca99d857c72241a9f3f092887067

--- Extensions/OneLine/OneLine/Editor/Drawers/CustomDrawer.cs
+++ Extensions/OneLine/OneLine/Editor/Drawers/CustomDrawer.cs
@@ -10,11 +10,10 @@ using System.Text.RegularExpressions;
 namespace OneLine {
     internal class CustomDrawer : SimpleFieldDrawer {
 
-        private readonly Dictionary<string, PropertyDrawer> drawers = new Dictionary<string, PropertyDrawer>();
-        private static Dictionary<Type, Type> customDrawers;
+        private CustomPropertyDrawers drawers = new CustomPropertyDrawers();
 
         public bool HasCustomDrawer(SerializedProperty property){
-            return GetCustomPropertyDrawerFor(property) != null;
+            return drawers.GetCustomPropertyDrawerFor(property) != null;
         }
 
         public override void Draw(Rect rect, SerializedProperty property) {
@@ -22,7 +21,7 @@ namespace OneLine {
         }
 
         private void DrawProperty(Rect rect, SerializedProperty property){
-            var drawer = GetCustomPropertyDrawerFor(property);
+            var drawer = drawers.GetCustomPropertyDrawerFor(property);
             if (drawer != null) {
                 drawer.OnGUI(rect, property, GUIContent.none);
             }
@@ -30,65 +29,6 @@ namespace OneLine {
                 var message = "[OneLine] Can not draw CustomPropertyDrawer for `{0}` at property path `{1}";
                 throw new Exception(string.Format(message, property.type, property.propertyPath));
             }
-        }
-
-        private static readonly Regex REGEXP_ARRAY_INDEX = new Regex(".data\\[\\d+\\]");
-
-        private PropertyDrawer GetCustomPropertyDrawerFor(SerializedProperty property){
-            var key = REGEXP_ARRAY_INDEX.Replace(property.propertyPath, "");
-            PropertyDrawer result = null;
-
-            if (! drawers.TryGetValue(key, out result)){
-                result = CreatePropertyDrawerFor(property);
-                drawers[key] = result;
-            }
-
-            return result;
-        }
-
-        private PropertyDrawer CreatePropertyDrawerFor(SerializedProperty property){
-            if (customDrawers == null){
-                customDrawers = findAllCustomDrawers();
-            }
-
-            var propertyType = property.GetRealType();
-            if (propertyType == null) return null;
-
-            Type drawerType = null;
-            Attribute drawerAttribute = null;
-            if (! customDrawers.TryGetValue(propertyType, out drawerType)) {
-                var attributes = property.GetCustomAttributes();
-                foreach (var attribute in attributes){
-                    if (customDrawers.TryGetValue(attribute.GetType(), out drawerType)){
-                        drawerAttribute = attribute;
-                        break;
-                    }
-                }
-            }
-
-            if (drawerType == null) return null;
-
-            var drawer = Activator.CreateInstance(drawerType) as PropertyDrawer;
-            drawer.SetAttribute(drawerAttribute);
-            return drawer;
-        }
-
-
-        private static Dictionary<Type, Type> findAllCustomDrawers(){
-            var result = new Dictionary<Type, Type>();
-
-            var entries = from assembly in AppDomain.CurrentDomain.GetAssemblies()
-                          from type in assembly.GetTypes()
-                          where type.IsSubclassOf(typeof(PropertyDrawer))
-                          where type != typeof(OneLinePropertyDrawer) && ! type.IsSubclassOf(typeof(OneLinePropertyDrawer))
-                          from attribute in type.GetCustomAttributes(typeof(CustomPropertyDrawer), true)
-                          select new {drawer = type, target = (attribute as CustomPropertyDrawer).GetTargetType()};
-
-            foreach (var entry in entries){
-                result[entry.target] = entry.drawer;
-            }
-
-            return result;
         }
 
     }
--- Extensions/OneLine/OneLine/Example/Details/Scripts/CustomPropertyDrawerExample.cs
+++ Extensions/OneLine/OneLine/Example/Details/Scripts/CustomPropertyDrawerExample.cs
@@ -8,40 +8,126 @@ using UnityEditor;
 namespace OneLine.Examples {
 [CreateAssetMenu(menuName = "OneLine/CustomPropertyDrawerExample")]
 public class CustomPropertyDrawerExample : ScriptableObject {
+    [Separator("Use With Children = True")]
     [SerializeField, OneLine]
-    private ThreeFields someFloats;
-    [SerializeField, OneLine, Range(0, 1)]
-    private float rootRange;
+    private DirectDrawer directDrawer;
     [SerializeField, OneLine]
-    private CustomField customDrawer;
+    private AttributeDrawer attributeDrawer;
+
+    [Separator("Use With Children = False")]
+    [SerializeField, OneLine]
+    private DirectWithoutChildren directWithoutChildren;
+    [SerializeField, OneLine]
+    private AttributeWithoutChildren attributeWithoutChildren;
+
+    #region Direct Custom Drawer
 
     [Serializable]
-    public class ThreeFields {
-        [SerializeField, Range(-50, 50)]
-        private int first;
+    public class DirectDrawer {
         [SerializeField]
-        private float second;
+        private Parent parent;
         [SerializeField]
-        private CustomField third;
+        private Child child;
+
+        [Serializable]
+        public class Parent {
+            [SerializeField]
+            private string first = "Default drawer";
+        }
+
+        [Serializable]
+        public class Child : Parent {
+            [SerializeField]
+            private string second = "Default drawer"; // Shall not be drown
+        }
+    }
+
+    #endregion
+
+    #region Custom Drawer on the Attribute
+
+    [Serializable]
+    public class AttributeDrawer {
+        [SerializeField, Range(0, 100)]
+        private float pureRange;
+        [SerializeField, Parent]
+        private AttributeExample parent;
+        [SerializeField, Child]
+        private AttributeExample child;
+
+        [Serializable]
+        public class AttributeExample {
+            [SerializeField]
+            private string first = "Default drawer";
+        }
+
+        public class Parent : PropertyAttribute {
+
+        }
+
+        public class Child : Parent {
+
+        }
+    }
+
+    #endregion
+
+    #region Do not drawer children
+
+    [Serializable]
+    public class DirectWithoutChildren {
+        [SerializeField]
+        private Parent parent;
+        [SerializeField]
+        private Child child;
+
+        [Serializable]
+        public class Parent {
+            [SerializeField]
+            private string first = "Default drawer";
+        }
+
+        [Serializable]
+        public class Child : Parent {
+        }
     }
 
     [Serializable]
-    public class CustomField {
-        [SerializeField]
-        private int first;
+    public class AttributeWithoutChildren {
+        [SerializeField, Range(0, 100)]
+        private float pureRange;
+        [SerializeField, Parent]
+        private AttributeExample parent;
+        [SerializeField, Child]
+        private AttributeExample child;
+
+        [Serializable]
+        public class AttributeExample {
+            [SerializeField]
+            private string first = "Default drawer";
+        }
+
+        public class Parent : PropertyAttribute {
+
+        }
+
+        public class Child : Parent {
+
+        }
     }
+
+    #endregion
 
 #if UNITY_EDITOR
-    [CustomPropertyDrawer(typeof(CustomField))]
+    [CustomPropertyDrawer(typeof(DirectDrawer.Parent), true)]
+    [CustomPropertyDrawer(typeof(AttributeDrawer.Parent), true)]
+    [CustomPropertyDrawer(typeof(DirectWithoutChildren.Parent), false)]
+    [CustomPropertyDrawer(typeof(AttributeWithoutChildren.Parent), false)]
     public class CustomFieldDrawer : PropertyDrawer {
         public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label) {
             rect = EditorGUI.PrefixLabel(rect, label);
 
-            property = property.FindPropertyRelative("first");
-
-            label = EditorGUI.BeginProperty(rect, label, property);
-            property.intValue = EditorGUI.IntSlider(rect, property.intValue, 1000, 2000);
-            EditorGUI.EndProperty();
+            EditorGUI.LabelField(rect, property.displayName + " is drown");
         }
     }
 }
