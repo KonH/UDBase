a7c1de52c6731420b5ce4654bbeef78393e0dc56
konh <konh@yandex.ru>
2018-04-24 23:39:53 +0700

    Refactoring: Config: Move common logics to FsJsonBaseConfig;

---
+++ Scripts/Controllers/Config/FsJsonBaseConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using UDBase.Utils.Json.Fullserializer;
+using UDBase.Controllers.LogSystem;
+
+namespace UDBase.Controllers.ConfigSystem {
+	
+	/// <summary>
+	/// Base fsJson config class for code reusage purposes
+	/// </summary>
+	public abstract class FsJsonBaseConfig : IConfig, ILogContext {
+		readonly Dictionary<Type, string> _nodeNames = new Dictionary<Type, string>();
+		
+		FsJsonNodeContainer _nodeContainer;
+		string              _configContent;
+
+		protected ILog _log;
+
+		public FsJsonBaseConfig(ILog log) {
+			_log = log;
+		}
+
+		protected void InitNodes(List<Config.ConfigItem> nodes) {
+			foreach ( var node in nodes ) {
+				AddNode(node.Type, node.Name);
+			}
+		}
+
+		protected void LoadContent(string configContent) {
+			_configContent = configContent;
+			_nodeContainer = new FsJsonNodeContainer(_configContent, _nodeNames, _log);
+			_log.MessageFormat(this, "Config content: \"{0}\"", _configContent);
+		}
+
+		protected void AddNode(Type type, string name) {
+			if( _nodeContainer == null ) {
+				if( !_nodeNames.ContainsKey(type) ) {
+					_nodeNames.Add(type, name);
+				} else {
+					_log.ErrorFormat(this, "Config: node already added: {0}!", type);
+				}
+			} else {
+				_nodeContainer.Add(type, name);
+			}
+		}
+
+		public virtual void Reload() { }
+
+		public virtual bool IsReady() => true;
+
+		public virtual T GetNode<T>() where T:IConfigSource {
+			return (_nodeContainer != null) ? _nodeContainer.LoadNode<T>(false) : default(T);
+		}
+	}
+}
--- Scripts/Controllers/Config/FsJsonResourcesConfig.cs
+++ Scripts/Controllers/Config/FsJsonResourcesConfig.cs
@@ -1,60 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UDBase.Utils.Json.Fullserializer;
+﻿using UnityEngine;
 using UDBase.Controllers.LogSystem;
 
 namespace UDBase.Controllers.ConfigSystem {
 
 	/// <summary>
-	/// Config controller, which uses JSON file serialization via Fullserializer
+	/// Config controller, which uses JSON file (located in Resources) serialization via Fullserializer
 	/// </summary>
-	public sealed class FsJsonResourcesConfig : IConfig, ILogContext {
-		readonly string                   _fileName;
-		readonly Dictionary<Type, string> _nodeNames = new Dictionary<Type, string>();
-		
-		FsJsonNodeContainer _nodeContainer;
-		string              _configContent;
+	public sealed class FsJsonResourcesConfig : FsJsonBaseConfig, ILogContext {
 
-		ILog _log;
-
-		public FsJsonResourcesConfig(Config.JsonSettings settings, ILog log) {
-			_log = log;
-			_fileName = settings.FileName;
-			var config = Resources.Load(_fileName) as TextAsset;
+		public FsJsonResourcesConfig(Config.JsonSettings settings, ILog log) : base(log) {
+			var fileName = settings.FileName;
+			var config = Resources.Load(fileName) as TextAsset;
 			if( config ) {
-				_configContent = config.text;
-				_nodeContainer = new FsJsonNodeContainer(_configContent, _nodeNames, _log);
+				LoadContent(config.text);
 			} else {
-				_log.ErrorFormat(this, "Can't read config file from Resources/{0}",  _fileName);
+				_log.ErrorFormat(this, "Can't read config file from Resources/{0}",  fileName);
 			}
-			_log.MessageFormat(this, "Config content: \"{0}\"", _configContent);
-			foreach ( var item in settings.Items ) {
-				AddNode(item.Type, item.Name);
-			}
-		}
-
-		FsJsonResourcesConfig AddNode(Type type, string name) {
-			if( _nodeContainer == null ) {
-				if( !_nodeNames.ContainsKey(type) ) {
-					_nodeNames.Add(type, name);
-				} else {
-					_log.ErrorFormat(this, "Config: node already added: {0}!", type);
-				}
-			} else {
-				_nodeContainer.Add(type, name);
-			}
-			return this;
-		}
-
-		public void Reload() {
-			// Isn't used
-		}
-
-		public bool IsReady() => true;
-
-		public T GetNode<T>() where T:IConfigSource {
-			return (_nodeContainer != null) ? _nodeContainer.LoadNode<T>(false) : default(T);
+			InitNodes(settings.Items);
 		}
 	}
 }
