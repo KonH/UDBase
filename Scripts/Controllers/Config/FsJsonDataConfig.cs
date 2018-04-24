ebc1cee42e348a16d2e76508798a49316f327e64
konh <konh@yandex.ru>
2018-04-24 23:40:06 +0700

    Feature: FsJsonDataConfig;

---
+++ Scripts/Controllers/Config/FsJsonDataConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using UnityEngine;
+using UDBase.Controllers.LogSystem;
+using UDBase.Utils;
+
+namespace UDBase.Controllers.ConfigSystem {
+
+	/// <summary>
+	/// Config fallback controller, which uses JSON file (located in Application.persistantDataPath) serialization via Fullserializer
+	/// </summary>
+	public sealed class FsJsonDataConfig : FsJsonBaseConfig, ILogContext {
+
+		public FsJsonDataConfig(Config.JsonSettings settings, ILog log) : base(log) {
+			var filePath = Path.Combine(Application.persistentDataPath, settings.FileName + ".json");
+			var configFileContents = IOTool.ReadAllText(filePath, true);
+			if( !string.IsNullOrEmpty(configFileContents) ) {
+				LoadContent(configFileContents);
+			} else {
+				_log.MessageFormat(this, "Can't read config file from '{0}'",  filePath);
+			}
+			InitNodes(settings.Items);
+		}
+	}
+}
