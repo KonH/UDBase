d94a3e40487ed6648af071c16ec91e79475d010b
konh <konh@yandex.ru>
2018-05-03 17:14:04 +0700

    Disabe warnings in FullSerializer;

--- Extensions/FullSerializer/Source/Converters/Unity/Keyframe_DirectConverter.cs
+++ Extensions/FullSerializer/Source/Converters/Unity/Keyframe_DirectConverter.cs
@@ -16,8 +16,10 @@ namespace FullSerializer.Internal.DirectConverters {
 
             result += SerializeMember(serialized, null, "time", model.time);
             result += SerializeMember(serialized, null, "value", model.value);
-            result += SerializeMember(serialized, null, "tangentMode", model.tangentMode);
-            result += SerializeMember(serialized, null, "inTangent", model.inTangent);
+		#pragma warning disable CS0618 // Type or member is obsolete
+			result += SerializeMember(serialized, null, "tangentMode", model.tangentMode);
+		#pragma warning restore CS0618 // Type or member is obsolete
+			result += SerializeMember(serialized, null, "inTangent", model.inTangent);
             result += SerializeMember(serialized, null, "outTangent", model.outTangent);
 
             return result;
@@ -34,9 +36,11 @@ namespace FullSerializer.Internal.DirectConverters {
             result += DeserializeMember(data, null, "value", out t1);
             model.value = t1;
 
-            var t2 = model.tangentMode;
-            result += DeserializeMember(data, null, "tangentMode", out t2);
+		#pragma warning disable CS0618 // Type or member is obsolete
+			var t2 = model.tangentMode;
+			result += DeserializeMember(data, null, "tangentMode", out t2);
             model.tangentMode = t2;
+		#pragma warning restore CS0618 // Type or member is obsolete
 
             var t3 = model.inTangent;
             result += DeserializeMember(data, null, "inTangent", out t3);
