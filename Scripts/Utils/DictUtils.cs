8a8a193618a90869c8d58e998e7278bbc90da95a
konh <konh@yandex.ru>
2018-06-21 21:15:29 +0700

    Added common dict utils;

---
+++ Scripts/Utils/DictUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace UDBase.Utils {
+	/// <summary>
+	/// Common dictionary utils
+	/// </summary>
+	public static class DictUtils {
+
+		/// <summary>
+		/// Return value by given key or default value if it isn't exists.
+		/// </summary>
+		public static TValue GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key) {
+			TValue value;
+			if ( dict.TryGetValue(key, out value) ) {
+				return value;
+			}
+			return default(TValue);
+		}
+	}
+}
