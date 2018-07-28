c231b24fde70b7a9bc5eebb594ada6b0c31bb407
konh <konh@yandex.ru>
2018-07-28 16:59:43 +0700

    Feature: RandomUtils.GetItem with elements weight;

--- Scripts/Utils/RandomUtils.cs
+++ Scripts/Utils/RandomUtils.cs
@@ -7,7 +7,8 @@ namespace UDBase.Utils {
 	/// Set of random helper methods
 	/// </summary>
 	public static class RandomUtils {
-		static int Range(int min, int max) => UnityEngine.Random.Range(min, max);
+		static int   Range(int min, int max)     => UnityEngine.Random.Range(min, max);
+		static float Range(float min, float max) => UnityEngine.Random.Range(min, max);
 
 		/// <summary>
 		/// Returns random value in [min, max] interval without exclusion items
@@ -53,6 +54,48 @@ namespace UDBase.Utils {
 		public static T GetItem<T>(T[] items) {
 			if ( (items != null) && (items.Length > 0) ) {
 				return items[Range(0, items.Length)];
+			}
+			return default(T);
+		}
+
+		/// <summary>
+		/// Returns random value from List collection with given weights of elements
+		/// </summary>
+		public static T GetItem<T>(List<T> items, List<float> weights) {
+			if ( (items != null) && (weights != null) && (items.Count > 0) && (items.Count == weights.Count) ) {
+				var maxValue = 0.0f;
+				foreach ( var w in weights ) {
+					maxValue += w;
+				}
+				var randValue = Range(0, maxValue);
+				var curValue = 0.0f;
+				for ( var i = 0; i < items.Count; i++ ) {
+					curValue += weights[i];
+					if ( randValue <= curValue ) {
+						return items[i];
+					}
+				}
+			}
+			return default(T);
+		}
+
+		/// <summary>
+		/// Returns random value from Array collection with given weights of elements
+		/// </summary>
+		public static T GetItem<T>(T[] items, float[] weights) {
+			if ( (items != null) && (weights != null) && (items.Length > 0) && (items.Length == weights.Length) ) {
+				var maxValue = 0.0f;
+				foreach ( var w in weights ) {
+					maxValue += w;
+				}
+				var randValue = Range(0, maxValue);
+				var curValue = 0.0f;
+				for ( var i = 0; i < items.Length; i++ ) {
+					curValue += weights[i];
+					if ( randValue <= curValue ) {
+						return items[i];
+					}
+				}
 			}
 			return default(T);
 		}
