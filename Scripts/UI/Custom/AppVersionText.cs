d04cbacf7af5a19888d6dbf12e0177247f137caf
konh <konh@yandex.ru>
2018-09-04 20:14:50 +0700

    Feature: add AppVersionText;

---
+++ Scripts/UI/Custom/AppVersionText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UDBase.UI.Custom {
+	[RequireComponent(typeof(Text))]
+	public class AppVersionText : MonoBehaviour {
+		[Tooltip("Must contains {0} placeholder")]
+		public string VersionFormat;
+
+		void Start() {
+			var text = GetComponent<Text>();
+			text.text = 
+				string.IsNullOrEmpty(VersionFormat)
+				? Application.version
+				: string.Format(VersionFormat, Application.version);
+		}
+	}
+}
